f5118d1426bda496fe074acd82f93d99c4ad130e
Enrico Marongiu <emarongiu@it.tiscali.com>
2015-05-18 17:54:10 +0200

    init behapi

---
+++ features/bootstrap/ApiContext.php
@@ -0,0 +1,278 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Hook\Scope\AfterFeatureScope;
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Behat\Behat\Hook\Scope\BeforeFeatureScope;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\TransferException;
+/**
+ * Description of ApiContext
+ *
+ * @author cernio
+ */
+class ApiContext implements Context, SnippetAcceptingContext{
+        public $_client;
+
+    /**
+     *
+     * @todo gestire meglio gli header 
+     */
+    public $loggedUser;
+    public $headers = array();
+    public $_response;
+    public $_bodyDecoded;
+    public $_jsonStore;
+
+    /**
+     * @BeforeSuite
+     */
+    public static function prepare(BeforeSuiteScope $scope) {
+        // prepare system for test suite
+        // before it runs
+    }
+
+    /**
+     * @BeforeFeature
+     * @param \JYL\Bundle\JYLApiBundle\Features\Context\BeforeFeature $scope
+     */
+    public static function prepareFeature(BeforeFeatureScope $scope) {
+        //da eseguire PRIMA di un file .feature
+    }
+
+    /**
+     *
+     * @AfterFeature
+     * @param \JYL\Bundle\JYLApiBundle\Features\Context\AfterFeature $scope
+     */
+    public static function closeFeature(AfterFeatureScope $scope) {
+        //da eseguire alla fine di un file .feature
+    }
+
+    /**
+     * @BeforeScenario @database
+     */
+    public function prepareDB(BeforeScenarioScope $scope) {
+        // clean database after scenarios,
+        // tagged with @database
+    }
+
+    /**
+     * @AfterScenario @database
+     */
+    public function cleanDB(AfterScenarioScope $scope) {
+        // clean database after scenarios,
+        // tagged with @database
+    }
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct($baseUrl) {
+
+//        $baseUrl=$params['baseUrl'];
+        $this->_client = new Client(['base_url' => $baseUrl]);
+    }
+
+    /**
+     * @Then lo status code deve essere :arg1
+     */
+    public function loStatusCodeDeveEssere($httpStatus) {
+        if ((string) $this->_response->getStatusCode() !== $httpStatus) {
+            throw new \Exception('HTTP code does not match ' . $httpStatus .
+            ' (actual: ' . $this->_response->getStatusCode() . ')' . substr($this->_response->getBody(), 0, 10000));
+        }
+    }
+
+    /**
+     * @Then il body e JSON
+     */
+    public function ilBodyEJson() {
+        $body = $this->_response->json();
+        $this->_bodyDecoded = $body;
+        $this->_jsonStore = new JsonStore($body);
+    }
+
+    /**
+     * @When faccio GET su :url
+     * @Given che faccio GET su :url
+     */
+    public function chefaccioGetSu($url) {
+        try {
+            if (!empty($this->loggedUser)) {
+                $response = $this->_client->get($url, [
+                    'auth' => [$this->loggedUser, ""]
+                ]);
+            } else {
+                $response = $this->_client->get($url);
+            }
+
+
+
+            $this->_response = $response;
+//            throw new \Exception("prova");
+        } catch (TransferException $e) {
+            if ($e->hasResponse()) {
+                $this->_response = $e->getResponse();
+            } else {
+                throw $e;
+            }
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * @Then /^la response deve contenere la property "([^"]*)"/ 
+     */
+    public function laResponseDeveContenereLaProprieta($arg1) {
+        $data = $this->_response->json();
+        if (empty($data)) {
+            throw new \Exception("JSON is empty\n" . $this->_response->getBody(true));
+        }
+        if (!isset($data[$arg1])) {
+            throw new \Exception("Property '" . $arg1 . "' is not set!\n");
+        }
+    }
+
+    /**
+     * @Then la property :arg1 deve contenere un box :arg2 e la qstring_params deve contenere :arg3
+     */
+    public function laPropertyDeveContenereUnBoxELaQstringParamsDeveContenere($arg1, $arg2, $arg3) {
+        $data = json_decode($this->_response->getBody(true));
+
+        $data = $this->_response->json();
+        $esisteInfobox = false;
+        $esisteCampo = false;
+        if (!empty($data)) {
+            foreach ($data[$arg1] as $tipo => $value) {
+                $infobox = $value['box'][0];
+                if ($infobox['service'] == $arg2) {
+                    $esisteInfobox = true;
+                    $qstringParams = $infobox['qstring_params'];
+                    if (array_key_exists($arg3, $qstringParams)) {
+                        $esisteCampo = true;
+                    }
+                }
+            }
+        }
+        if (!$esisteInfobox) {
+            throw new \Exception("Not infobox '" . $arg2 . "' !\n");
+        }
+
+        if (!$esisteCampo) {
+            throw new \Exception("Not field'" . $arg3 . "' !\n");
+        }
+    }
+
+//    /**
+//     * la property :$propertyName deve contenere un box :$propertyValue con valore nella qstring di :arg3
+//     */
+//    public function laPropertyDeveContenereUnBoxConValoreNellaQstringDi($propertyName, $propertyValue, $arg3){
+//    
+//        $data = json_decode($this->_response->getBody(true));
+//        
+//        $data = $this->_response->json();
+//        if (!empty($data)) {
+//            foreach ($data[$propertyName] as $tipo=>$value){
+//                var_dump($tipo);
+//                var_dump($value);
+//            }
+//            
+//            
+//        } else {
+//            throw new Exception("Response was not JSON\n" . $this->_response->getBody(true));
+//        }
+//        
+//        
+//    }
+    /**
+     * @Then JSONPATH :jsonpath è uguale a :valore
+     */
+    public function jsonpathEUgualeA($jsonpath, $valore) {
+
+        $res = $this->_jsonStore->get($this->_bodyDecoded, $jsonpath);
+        //todo estendi per valori multipli
+//        if(is_array($valore)){ 
+//            foreach($valore as $val){
+//                
+//            }
+//        }
+        print "jsonpath res:\n";
+        print_r($res);
+    }
+
+    /**
+     * @Then JSONPATH :arg1 contiene una property :arg2
+     */
+    public function jsonpathContieneUnaProperty($arg1, $arg2) {
+        $res = $this->_jsonStore->get($arg1);
+        foreach ($res as $resItem) {
+//            print_r($resItem[$arg2]);
+            if (empty($resItem[$arg2])) {
+                print_r($resItem);
+                throw new \Exception("item $arg2 not found ");
+            }
+        }
+    }
+    
+        /**
+     * @Then JSONPATH :arg1 contiene un dato di tipo :arg2
+     * @Then la proprieta JSON :arg1 contiene un dato di tipo :arg2
+     */
+    public function jsonpathContieneUnDatoDiTipo($arg1, $arg2)
+    {
+        $res = $this->_jsonStore->get($arg1);
+        if(empty($res)){
+            throw new \Exception("no items matching criteria ");
+        }
+//        print_r($res);
+        foreach ($res as $resItem) {
+//            print_r($resItem[$arg2]);
+            if (empty($resItem)) {
+                throw new \Exception("item not found ");
+            }
+            switch($arg2){
+                case 'integer':
+                    if(!is_integer($resItem)){
+                        throw new \Exception("not integer");
+                    }
+                    break;
+                case 'float': 
+                case 'double': 
+                    if(!is_double($resItem)){
+                        throw new \Exception("not double");
+                    }
+                    break;
+                case 'string':
+                    if(!is_string($resItem)){
+                        throw new \Exception("not string");
+                    }
+                    break;
+                case 'url':
+                    if(filter_var($resItem, FILTER_VALIDATE_URL) === FALSE){
+                        throw new \Exception("not a valid url: ". $resItem);
+                    }
+                    break;
+                default:
+                    throw new \Exception("case not implemented");
+                    
+            }
+            
+        }
+    }
+
+}
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -5,9 +5,6 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
-class PendingException extends \Exception
-{
-}
 /**
  * Defines application features from the specific context.
  */
@@ -22,73 +19,5 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
-    }
-
-
-   /**
-     * @Given che esiste :arg1 con notifiche digest
-     */
-    public function cheEsisteConNotificheDigest($arg1)
-    {
-	print "esiste $arg1";
-	return true;
-    }
-
-    /**
-     * @Given che esiste :arg1
-     */
-    public function cheEsiste($arg1)
-    {
-	print "esiste $arg1";
-	return true;
-    }
-
-    /**
-     * @Given :arg1 è un utente attivo
-     */
-    public function eUnUtenteAttivo($arg1)
-    {
-	print "esiste ed èattivo $arg1";
-	return true;
-    }
-
-    /**
-     * @When :arg1 segue :arg2
-     */
-    public function segue($arg1, $arg2)
-    {
-	print "$arg1 -> $arg2";
-    }
-
-    /**
-     * @Then :arg1 riceve una mail di notifica diretta
-     */
-    public function riceveUnaMailDiNotificaDiretta($arg1)
-    {
-	print	"$arg1 ha ricevuto una mail diretta";
-    }
-
-    /**
-     * @Then :arg1 non riceve una mail di notifica digest
-     */
-    public function nonRiceveUnaMailDiNotificaDigest($arg1)
-    {
-        print "$arg1 ha ricevuto una mail digest";
-    }
-    /**
-     * @Given che esiste :arg1 con notifiche dirette
-     */
-    public function cheEsisteConNotificheDirette($arg1)
-    {
-//	print "$arg1 ha notifiche dirette";
-    }
-
-
-    /**
-     * @Given che :arg1 non segue :arg2
-     */
-    public function cheNonSegue($arg1, $arg2)
-    {
-        //throw new PendingException();
     }
 }
